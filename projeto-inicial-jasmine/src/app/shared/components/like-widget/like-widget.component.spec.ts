214fcd1f3b5d5e7569a12cd635f1675f31acf509
André J. Bez Birolo <andrecentrobrasileiro@outlook.com>
2022-03-31 16:19:02 -0300

    feat(like-widget): implemented a spy on test

--- projeto-inicial-jasmine/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ projeto-inicial-jasmine/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -34,13 +34,11 @@ describe('LikeWidgetComponent', () => {
   });
 
   it(`#${LikeWidgetComponent.prototype.like.name}
-  should trigger emission when called`, done => {
+  should trigger emission when called`, () => {
+    spyOn(component.liked, 'emit');
     fixture.detectChanges();
-    component.liked.subscribe(() => {
-      expect(true).toBeTrue();
-      done();
-    });
     component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
   });
 
 });
