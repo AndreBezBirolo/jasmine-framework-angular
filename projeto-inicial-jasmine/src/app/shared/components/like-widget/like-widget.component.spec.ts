14d24a3d2ac3916a16440a02d46c51a85d04cfca
André J. Bez Birolo <andrecentrobrasileiro@outlook.com>
2022-03-31 16:10:05 -0300

    feat(like-widget): implemented a test's in component

--- projeto-inicial-jasmine/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ projeto-inicial-jasmine/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,5 +1,46 @@
 import { LikeWidgetComponent } from './like-widget.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LikeWidgetModule } from './like-widget.module';
 
 describe('LikeWidgetComponent', () => {
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
+  let component: LikeWidgetComponent = null;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        LikeWidgetModule
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LikeWidgetComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('Should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Should auto generate ID when id input property is missing', () => {
+    fixture.detectChanges();
+    expect(component.id).toBeTruthy();
+  });
+
+  it('Shouldnt generate ID when id input property is presente', () => {
+    const ID = 'someID';
+    component.id = ID;
+    fixture.detectChanges();
+    expect(component.id).toBe(ID);
+  });
+
+  it(`#${LikeWidgetComponent.prototype.like.name}
+  should trigger emission when called`, done => {
+    fixture.detectChanges();
+    component.liked.subscribe(() => {
+      expect(true).toBeTrue();
+      done();
+    });
+    component.like();
+  });
 
 });
