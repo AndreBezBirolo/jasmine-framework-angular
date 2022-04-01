e54e1540c1abc6ef73a72e2b8da652e803643ff8
André J. Bez Birolo <andrecentrobrasileiro@outlook.com>
2022-04-01 13:36:54 -0300

    feat(photo-frame): added aria-label tests

--- projeto-avancado-jasmine/src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ projeto-avancado-jasmine/src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -52,4 +52,18 @@ describe(PhotoFrameComponent.name, () => {
     expect(element.textContent.trim()).toBe('1');
   });
 
+  it(`Should update aria-label when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+    component.likes++;
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('1: people liked');
+  });
+
+  it(`Should have aria-label with 0 (@Input likes)`, () => {
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('0: people liked');
+  });
+
 });
