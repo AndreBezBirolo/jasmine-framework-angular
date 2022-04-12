1b59e7bc1e086943447a8ceae8f44df2f8a225b3
André J. Bez Birolo <andrecentrobrasileiro@outlook.com>
2022-04-12 15:04:49 -0300

    feat(action-direction): create a directive to simply the function to click action and tested if directive works

---
+++ projeto-avancado-jasmine/src/app/shared/directives/action/action.directive.spec.ts
@@ -0,0 +1,42 @@
+import { ActionDirective } from './action.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ActionModule } from './action.module';
+
+describe(ActionDirective.name, () => {
+  let fixture: ComponentFixture<ActionDirectiveTestComponent>;
+  let component: ActionDirectiveTestComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ActionDirectiveTestComponent],
+      imports: [ActionModule]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ActionDirectiveTestComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('(DOM) (@Output appAction) should emit event with payload when ENTER key is pressed', () => {
+    const divEl: HTMLElement = fixture.nativeElement.querySelector('.dummy-component');
+    const event = new KeyboardEvent('keyup', { key: 'Enter' });
+    divEl.dispatchEvent(event);
+    expect(component.hasEvent()).toBe(true);
+  });
+});
+
+@Component({
+  template: `
+    <div class="dummy-component" (appAction)="actionHandler($event)"></div>`
+})
+class ActionDirectiveTestComponent {
+  private event: Event = null;
+
+  public actionHandler(event: Event): void {
+    this.event = event;
+  }
+
+  public hasEvent(): boolean {
+    return !!this.event;
+  }
+}
