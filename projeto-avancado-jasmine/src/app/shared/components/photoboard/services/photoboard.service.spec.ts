700789f3c4b6a85300a3d89850e9310ef60e17d5
André J. Bez Birolo <andrecentrobrasileiro@outlook.com>
2022-04-18 16:36:48 -0300

    feat(photo-board): implemented a test with httpController

--- projeto-avancado-jasmine/src/app/shared/components/photoboard/services/photoboard.service.spec.ts
+++ projeto-avancado-jasmine/src/app/shared/components/photoboard/services/photoboard.service.spec.ts
@@ -1,16 +1,55 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PhotoboardService } from './photoboard.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from '../../../../../environments/environment';
 
-describe('PhotoboardService', () => {
+const mockData = {
+  api: `${environment.API_URL}/photos`,
+  data: [
+    {
+      id: 1,
+      description: 'example 1',
+      src: ''
+    },
+    {
+      id: 2,
+      description: 'example 2',
+      src: ''
+    },
+    {
+      id: 3,
+      description: 'example 3',
+      src: ''
+    },
+  ]
+}
+
+describe(PhotoboardService.name, () => {
   let service: PhotoboardService;
+  let httpController: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PhotoboardService]
+    });
     service = TestBed.inject(PhotoboardService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  it('Should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it(`#${PhotoboardService.prototype.getPhotos.name} should return photos with description in uppercase`, done => {
+    service.getPhotos().subscribe((photos) => {
+      expect(photos[ 0 ].description).toBe('EXAMPLE 1');
+      expect(photos[ 1 ].description).toBe('EXAMPLE 2');
+      done();
+    })
+    httpController
+      .expectOne(mockData.api)
+      .flush(mockData.data);
+  })
 });
--- projeto-avancado-jasmine/src/app/shared/components/photoboard/services/photoboard.service.ts
+++ projeto-avancado-jasmine/src/app/shared/components/photoboard/services/photoboard.service.ts
@@ -3,13 +3,20 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Photo } from '../interfaces/photo';
 import { environment } from '../../../../../environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class PhotoboardService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
 
   public getPhotos(): Observable<Photo[]> {
-    return this.httpClient.get<Photo[]>(`${environment.API_URL}/photos`);
+    return this.httpClient.get<Photo[]>(`${environment.API_URL}/photos`)
+      .pipe(map(photos => {
+        return photos.map(photo => {
+          return { ...photo, description: photo.description.toLocaleUpperCase() }
+        })
+      }))
   }
 }
