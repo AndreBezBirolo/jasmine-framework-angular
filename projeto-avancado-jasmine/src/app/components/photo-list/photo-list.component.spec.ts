c2734373fbb213cf12cd5e22bb998a23926a6b72
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2022-04-18 12:03:16 -0300

    feat(photo-list): test in page scope

--- projeto-avancado-jasmine/src/app/components/photo-list/photo-list.component.spec.ts
+++ projeto-avancado-jasmine/src/app/components/photo-list/photo-list.component.spec.ts
@@ -1,25 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PhotoListComponent } from './photo-list.component';
+import { PhotoListModule } from './photo-list.module';
+import { HttpClientModule } from '@angular/common/http';
 
-describe('PhotoListComponent', () => {
+describe(PhotoListComponent.name, () => {
   let component: PhotoListComponent;
   let fixture: ComponentFixture<PhotoListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PhotoListComponent ]
+      imports: [PhotoListModule, HttpClientModule],
     })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
+      .compileComponents();
     fixture = TestBed.createComponent(PhotoListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should create component', () => {
     expect(component).toBeTruthy();
   });
 });
